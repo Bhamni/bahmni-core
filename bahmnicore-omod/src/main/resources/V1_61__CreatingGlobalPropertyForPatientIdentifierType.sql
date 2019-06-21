a17d173aeebd290426959ac28d6cc23d38cc1909
Samuel Male <samuelsmalek@gmail.com>
2019-06-21 11:51:33 +0300

    BAH-828:Unable to apply Bahmni Liquibase changes on a 'fresh' OpenMRS database.

--- bahmnicore-omod/src/main/resources/V1_61__CreatingGlobalPropertyForPatientIdentifierType.sql
+++ bahmnicore-omod/src/main/resources/V1_61__CreatingGlobalPropertyForPatientIdentifierType.sql
@@ -1,2 +1,3 @@
-select uuid from patient_identifier_type where name = 'Bahmni Id' into @uuid;
-UPDATE global_property SET property_value = @uuid WHERE property='emr.primaryIdentifierType';
+set @property = 'emr.primaryIdentifierType', @propertyValue = '';
+select uuid from patient_identifier_type where name = 'Bahmni Id' into @propertyValue;
+INSERT into global_property (uuid, property, property_value) VALUES (uuid(), @property, @propertyValue) ON DUPLICATE KEY UPDATE property_value = @propertyValue; 
