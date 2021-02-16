6b1018cc4f527a4eb2a5a8ad716e9ec2406288c0
Rakesh Kumar <rakesh.kumar@thoughtworks.com>
2021-02-16 17:30:19 +0530

    BAH-1157 | Rakesh | Patient Migration - CSV Upload Date format support for `/` separator char

--- admin/src/main/java/org/bahmni/module/admin/csv/utils/CSVUtils.java
+++ admin/src/main/java/org/bahmni/module/admin/csv/utils/CSVUtils.java
@@ -1,17 +1,21 @@
 package org.bahmni.module.admin.csv.utils;
 
 import org.bahmni.csv.KeyValue;
+import org.bahmni.csv.exception.MigrationException;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 public class CSVUtils {
 
     public static final String ENCOUNTER_DATE_PATTERN = "yyyy-M-d";
+
+    public static final List<String> SUPPORTED_DATE_PATTERNS = Arrays.asList("yyyy-M-d", "yyyy/M/d");
 
     public static String[] getStringArray(List<KeyValue> keyValueList) {
         List<String> stringList = new ArrayList<>();
@@ -30,10 +34,19 @@ public class CSVUtils {
     }
 
     public static Date getDateFromString(String dateString) throws ParseException {
-        // All csv imports use the same date format
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(ENCOUNTER_DATE_PATTERN);
-        simpleDateFormat.setLenient(false);
-        return simpleDateFormat.parse(dateString);
+            // Check all supported date formats
+            for (String formatString : SUPPORTED_DATE_PATTERNS)
+            {
+                try
+                {
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(formatString);
+                    simpleDateFormat.setLenient(false);
+                    return simpleDateFormat.parse(dateString);
+                }
+                catch (ParseException e) {}
+            }
+            // if pattern is not supported throw the parse error
+            throw new MigrationException("Unparseable date "+ dateString);
     }
 
     public static Date getTodayDate() throws ParseException {
